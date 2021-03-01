06684041f7f715396ed8936215aa4a1e77205663
Srikar KSV <srikar1awesome@gmail.com>
2021-03-01 13:56:19 +0530

    📦 NEW: Prototyped the code to shorten URLs

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,20 @@
+const URL = require('../models/URL');
+
 exports.home = (req, res) => {
-  res.render('home', { title: 'Home' });
+  let shorturl;
+  if (req.body?.shorturl) {
+    shorturl = `${req.headers.host}/${req.body.shorturl}`;
+  }
+  res.render('home', { title: 'Home', shorturl });
+};
+
+exports.shorten = async (req, res, next) => {
+  const shortenedURL = await URL.create(req.body);
+  req.body.shorturl = shortenedURL.urlId;
+  next();
+};
+
+exports.redirectPage = async (req, res) => {
+  const originalURL = await URL.findOne({ urlId: req.params.id });
+  res.redirect(originalURL.url);
 };
--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -4,5 +4,7 @@ const homeController = require('../controllers/homeController');
 const router = express.Router();
 
 router.get('/', homeController.home);
+router.post('/shorten', homeController.shorten, homeController.home);
+router.get('/:id', homeController.redirectPage);
 
 module.exports = router;
