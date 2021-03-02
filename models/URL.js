542bfaf8dd6f9f8d808c117c6e79a18f8d2f1171
Srikar KSV <srikar1awesome@gmail.com>
2021-03-02 21:20:19 +0530

    👌 IMPROVE: Added validator to the url field

--- models/URL.js
+++ models/URL.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
+const validator = require('validator');
 
 const urlSchema = new mongoose.Schema({
   url: {
     type: String,
     trim: true,
-    required: 'You need to provide a URL',
+    required: 'You need to provide a URL(s)',
+    validate: [validator.isURL, 'One of the URL(s) is not valid'],
   },
   urlId: String,
 });
