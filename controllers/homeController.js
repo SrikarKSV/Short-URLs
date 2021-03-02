9a529e5dae051ed19101e57e207132c7449419ed
Srikar KSV <srikar1awesome@gmail.com>
2021-03-02 09:53:58 +0530

    👌 IMPROVE: Now can shorten multiple urls

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,24 +3,30 @@ const URL = require('../models/URL');
 exports.home = (req, res) => {
   let shorturl;
   if (req.body?.shorturl) {
-    shorturl = `${req.headers.host}/${req.body.shorturl}`;
+    shorturl = req.body.shorturl.map((url) => ({
+      originalURL: url.url,
+      short: `${req.headers.host}/${url.urlId}`,
+    }));
   }
   res.render('home', { title: 'Home', shorturl });
 };
 
 exports.shorten = async (req, res, next) => {
-  const shortenedURL = await URL.create(req.body);
-  req.body.shorturl = shortenedURL.urlId;
+  const createPromise = req.body.url
+    .split(',')
+    .map((url) => URL.create({ url: url.trim() }));
+  const urlIds = await Promise.all(createPromise);
+  req.body.shorturl = urlIds;
   next();
 };
 
 exports.notFavicon = (req, res, next) => {
+  // skip the request if favicon
   if (req.params.id == 'favicon.ico') return;
   next();
 };
 
 exports.redirectPage = async (req, res) => {
-  console.log('HELLO??', req.params.id);
   const originalURL = await URL.findOne({ urlId: req.params.id });
   res.redirect(originalURL.url);
 };
