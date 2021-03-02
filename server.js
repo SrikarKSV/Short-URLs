8a58eb66134b9c95764fa0070116641fcf548160
Srikar KSV <srikar1awesome@gmail.com>
2021-03-02 15:08:47 +0530

    👌 IMPROVE: Imported the mongodb document

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ mongoose
     console.log('💥💥 There was an error connecting to DATABASE', err)
   );
 
+require('./models/URL');
+require('./models/UrlPage');
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, console.log(`Server started on http://localhost:${PORT}`));
