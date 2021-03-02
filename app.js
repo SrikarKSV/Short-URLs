20cfa0df5269d250adc446d696a6f0819ae4a74f
Srikar KSV <srikar1awesome@gmail.com>
2021-03-02 09:55:59 +0530

    📦 NEW: Setup session and flash

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const homeRouter = require('./routes/homeRouter');
 
@@ -15,6 +17,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Reading data from body into req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.flashes = req.flash();
+  next();
+});
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
