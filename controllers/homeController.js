4d4764c7ce2481aa10ce137852294443327962eb
Srikar KSV <srikar1awesome@gmail.com>
2021-03-02 15:09:36 +0530

    👌 IMPROVE: New links page generated for 24 hours

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,32 +1,39 @@
 const URL = require('../models/URL');
+const UrlPage = require('../models/UrlPage');
 
 exports.home = (req, res) => {
-  let shorturl;
-  if (req.body?.shorturl) {
-    shorturl = req.body.shorturl.map((url) => ({
-      originalURL: url.url,
-      short: `${req.headers.host}/${url.urlId}`,
-    }));
-  }
-  res.render('home', { title: 'Home', shorturl });
+  res.render('home', { title: 'Home' });
 };
 
-exports.shorten = async (req, res, next) => {
+exports.shorten = async (req, res) => {
   const createPromise = req.body.url
     .split(',')
     .map((url) => URL.create({ url: url.trim() }));
   const urlIds = await Promise.all(createPromise);
-  req.body.shorturl = urlIds;
-  next();
+  const urlsPage = await UrlPage.create({
+    urlIds: urlIds.map((id) => id._id),
+  });
+  req.flash('success', 'Link(s) have been shortened!');
+  res.status(201).redirect(`/links/${urlsPage.id}`);
+};
+
+exports.urlsPage = async (req, res) => {
+  const shorturls = await UrlPage.findOne({ id: req.params.token }).populate(
+    'urlIds'
+  );
+  console.log(shorturls);
+  const { urlIds } = shorturls;
+  res.render('links', { title: 'Links', urlIds });
+};
+
+// Redirecting from the shortened link
+exports.redirectPage = async (req, res) => {
+  const originalURL = await URL.findOne({ urlId: req.params.id });
+  res.redirect(originalURL.url);
 };
 
 exports.notFavicon = (req, res, next) => {
   // skip the request if favicon
   if (req.params.id == 'favicon.ico') return;
   next();
-};
-
-exports.redirectPage = async (req, res) => {
-  const originalURL = await URL.findOne({ urlId: req.params.id });
-  res.redirect(originalURL.url);
 };
--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -3,10 +3,9 @@ const homeController = require('../controllers/homeController');
 
 const router = express.Router();
 
-router
-  .route('/')
-  .get(homeController.home)
-  .post(homeController.shorten, homeController.home);
+router.route('/').get(homeController.home).post(homeController.shorten);
+
+router.get('/links/:token', homeController.urlsPage);
 
 router.get('/:id', homeController.notFavicon, homeController.redirectPage);
 
