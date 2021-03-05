652e0ac2b5f2102861e611f026c209e1a08cdda5
Srikar KSV <srikar1awesome@gmail.com>
2021-03-05 17:08:27 +0530

    🐛 FIX: Removed hardcoded url

--- server.js
+++ server.js
@@ -27,4 +27,4 @@ require('./models/UrlPage');
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, console.log(`Server started on http://localhost:${PORT}`));
+app.listen(PORT, console.log(`Server started on ${PORT}`));
