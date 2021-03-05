0ba162d4bb591baabe02b85aa9e524e4856a1009
Srikar KSV <srikar1awesome@gmail.com>
2021-03-05 11:12:58 +0530

    📦 NEW: Styled home page

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,15 @@ import '../css/main.css';
 
 const allRadioBtn = document.querySelectorAll('input[type="radio"]');
 const dateTimeInput = document.querySelector('#datetime');
+const customExpiryDateDiv = document.querySelector('.custom-expiry-date');
 
 allRadioBtn.forEach((radioBtn) => {
   radioBtn.addEventListener('input', (e) => {
-    dateTimeInput.disabled = !e.target.matches('#custom');
+    const toggleOpen = e.target.matches('#custom');
+    toggleOpen
+      ? customExpiryDateDiv.classList.add('custom-expiry-date--expand')
+      : customExpiryDateDiv.classList.remove('custom-expiry-date--expand');
+    dateTimeInput.disabled = !toggleOpen;
   });
 });
 
