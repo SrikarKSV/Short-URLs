f261ea45de4f1c417c81a00d0827d0d3f1f2f2f1
Srikar KSV <srikar1awesome@gmail.com>
2021-03-06 11:58:19 +0530

    🐛 FIX: Fixed date input issue with mobiles

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,20 @@ import 'flatpickr/dist/flatpickr.css';
 import '../css/main.css';
 
 const allRadioBtn = document.querySelectorAll('input[type="radio"]');
-const dateTimeInput = document.querySelector('#datetime');
 const customExpiryDateDiv = document.querySelector('.custom-expiry-date');
 const form = document.querySelector('.form');
-const extraLinks = document.querySelector('.extra-links');
+
+const moreThan10LinksHTML = `
+  <div class="flash flash--error extra-links">
+    <p>Don't shorten more than 10 links</p>
+  </div>
+`;
+
+const chooseExpiryDateHTML = `
+  <div class="flash flash--error enter-custom">
+    <p>Please select expiry date!</p>
+  </div>
+`;
 
 allRadioBtn.forEach((radioBtn) => {
   radioBtn.addEventListener('input', (e) => {
@@ -14,15 +24,18 @@ allRadioBtn.forEach((radioBtn) => {
     toggleOpen
       ? customExpiryDateDiv.classList.add('custom-expiry-date--expand')
       : customExpiryDateDiv.classList.remove('custom-expiry-date--expand');
-    dateTimeInput.disabled = !toggleOpen;
   });
 });
 
+// Checking values before submission
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const urls = e.target.url.value.split(',').map((url) => url.trim());
   if (urls.length > 10) {
-    extraLinks.hidden = false;
+    form.insertAdjacentHTML('beforebegin', moreThan10LinksHTML);
+  } else if (!allRadioBtn[0].checked && allRadioBtn[1].value === 'on') {
+    // if the sxpiry date is not selected
+    form.insertAdjacentHTML('beforebegin', chooseExpiryDateHTML);
   } else {
     e.target.submit();
   }
