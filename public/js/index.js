942688032d2a04e0ed30bdb4816a9e85faa1f109
Srikar KSV <srikar1awesome@gmail.com>
2021-03-06 14:00:29 +0530

    👌 IMPROVE: Changed strategy to show validation errors

--- public/js/index.js
+++ public/js/index.js
@@ -4,21 +4,10 @@ import '../css/main.css';
 
 const allRadioBtn = document.querySelectorAll('input[type="radio"]');
 const customExpiryDateDiv = document.querySelector('.custom-expiry-date');
+const extraLinks = document.querySelector('.extra-links');
+const customExpiryDate = document.querySelector('.enter-custom');
+
 const form = document.querySelector('.form');
-
-let isMoreThan10Present = false;
-const moreThan10LinksHTML = `
-  <div class="flash flash--error extra-links">
-    <p>Don't shorten more than 10 links</p>
-  </div>
-`;
-
-let ischooseExpiryPresent = false;
-const chooseExpiryDateHTML = `
-  <div class="flash flash--error enter-custom">
-    <p>Please select expiry date!</p>
-  </div>
-`;
 
 allRadioBtn.forEach((radioBtn) => {
   radioBtn.addEventListener('input', (e) => {
@@ -34,14 +23,10 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const urls = e.target.url.value.split(',').map((url) => url.trim());
   if (urls.length > 10) {
-    !isMoreThan10Present &&
-      form.insertAdjacentHTML('beforebegin', moreThan10LinksHTML);
-    isMoreThan10Present = true;
+    extraLinks.hidden = false;
   } else if (!allRadioBtn[0].checked && allRadioBtn[1].value === 'on') {
-    // if the sxpiry date is not selected
-    !ischooseExpiryPresent &&
-      form.insertAdjacentHTML('beforebegin', chooseExpiryDateHTML);
-    ischooseExpiryPresent = true;
+    // if the expiry date is not selected
+    customExpiryDate.hidden = false;
   } else {
     e.target.submit();
   }
